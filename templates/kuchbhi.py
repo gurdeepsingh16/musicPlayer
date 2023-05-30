cd0fca4488e80a832b58c8a6b6dfe3305e74e0d1
Harishdhiman <110805487+Harishdhiman@users.noreply.github.com>
2023-05-30 12:25:39 +0530

    add file

--- templates/kuchbhi.py
+++ templates/kuchbhi.py
@@ -1,11 +1,11 @@
-import re
+# importing the libraries
+import cv2
+import numpy as np
 
-s = 'GeeksforGeeks: A computer science  for geeks'
+# creating an array using np.full
+# 255 is code for white color
+array_created = np.full((500, 500, 3),
+						255, dtype = np.uint8)
 
-match = re.search(r'portal', s)
-
-if match is None:
-    print(False)
-else:
-    print('Start Index:', match.start())
-    print('End Index:', match.end())
+# displaying the image
+cv2.imshow("image", array_created)
